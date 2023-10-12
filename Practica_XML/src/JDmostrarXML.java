27960aff605cddfabf87326e4edccf15459496fb
avg0020 <avg0020@alu.medac.es>
2023-10-12 17:38:37 +0200

    CargarXML
    se han hecho los botones de cargar xml y mostrarlo en consola

---
+++ Practica_XML/src/JDmostrarXML.java
@@ -0,0 +1,57 @@
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JScrollPane;
+import javax.swing.ScrollPaneConstants;
+import javax.swing.JTextArea;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class JDmostrarXML extends JDialog {
+
+	private static final long serialVersionUID = 1L;
+	private final JPanel contentPanel = new JPanel();
+	private JScrollPane scrollPane;
+
+	/**
+	 * Create the dialog.
+	 */
+	public JDmostrarXML(JFPrincipal parent, boolean modal) {
+		super(parent,modal);
+		setBounds(100, 100, 450, 300);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setLayout(new FlowLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		{
+			JPanel buttonPane = new JPanel();
+			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			{
+				JButton okButton = new JButton("OK");
+				okButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						dispose();
+					}
+				});
+				okButton.setActionCommand("OK");
+				buttonPane.add(okButton);
+				getRootPane().setDefaultButton(okButton);
+			}
+		}
+		
+		scrollPane = new JScrollPane();
+		scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+		getContentPane().add(scrollPane, BorderLayout.CENTER);
+	}
+	
+	public void editarScrollPane(String text) {
+		
+	}
+
+}
--- Practica_XML/src/JFPrincipal.java
+++ Practica_XML/src/JFPrincipal.java
@@ -1,21 +1,33 @@
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.filechooser.FileNameExtensionFilter;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
-
 import java.awt.Font;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
 import java.awt.event.ActionEvent;
 
 public class JFPrincipal extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
-	String ruta;
+	String ruta="", xml="";
 
 	/**
 	 * Launch the application.
@@ -45,6 +57,8 @@ public class JFPrincipal extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
+		JDmostrarXML jd = new JDmostrarXML(this, true);
+		
 		JButton btnCargar = new JButton("Cargar XML");
 		btnCargar.setFont(new Font("Comic Sans MS", Font.PLAIN, 19));
 		btnCargar.setBounds(200, 101, 200, 50);
@@ -73,11 +87,46 @@ public class JFPrincipal extends JFrame {
 		contentPane.add(btnMostrar);
 		btnMostrar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				System.out.println(ruta);
+				if (ruta.isEmpty()) {					
+					JOptionPane.showMessageDialog(null,"No hay ningun XML","Alerta",JOptionPane.ERROR_MESSAGE);
+				} else {
+					try {
+						// Creamos la instancia de dbf
+						DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+						// Creamos un nuevo documento vacío a partir de dicha instancia
+						DocumentBuilder db = dbf.newDocumentBuilder();
+						// Creamos un objeto File con nuestro fichero XML
+						File fichXML = new File(ruta);
+						// Creamos el arbol DOM a partir del fichero
+						Document doc = db.parse(fichXML);
+						// normalize. Elimina nodos vacíos y combina nodos adyacentes en caso de que los
+						// hubiera
+						doc.getDocumentElement().normalize();
+						
+						xml="Elemento Raiz :" + doc.getDocumentElement().getNodeName()+"\n"+"----------";
+						System.out.println("Elemento Raiz :" + doc.getDocumentElement().getNodeName());
+						System.out.println("----------");
+
+						if (doc.hasChildNodes()) {
+							printNote(doc.getChildNodes(),xml);
+						}
+						
+						//Muestra el JDialog
+						jd.setVisible(true);
+					} catch (ParserConfigurationException | SAXException | IOException ex) {
+						ex.printStackTrace();
+					}
+				}
 			}
 		});
 		
 		JButton btnGuardar = new JButton("Guardar Valores");
+		btnGuardar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("A partir de aquí");
+				System.out.println(xml);
+			}
+		});
 		btnGuardar.setFont(new Font("Comic Sans MS", Font.PLAIN, 19));
 		btnGuardar.setBounds(200, 171, 200, 50);
 		contentPane.add(btnGuardar);
@@ -95,5 +144,40 @@ public class JFPrincipal extends JFrame {
 		});
 	}
 	
-	
+	public static String printNote(NodeList nodeList, String text) {
+
+		for (int count = 0; count < nodeList.getLength(); count++) {
+
+			Node tempNode = nodeList.item(count);
+			
+			// make sure it's element node.
+			if (tempNode.getNodeType() == Node.ELEMENT_NODE) {
+				text+="\nElemento: "+tempNode.getNodeName();
+				System.out.println("Elemento: "+tempNode.getNodeName());
+				// get node name and value
+				if (!tempNode.getTextContent().contains("\n")) {
+					text+="\n	Valor: " + tempNode.getTextContent();
+					System.out.println("	Valor: " + tempNode.getTextContent());
+				}
+				
+				if (tempNode.hasAttributes()) {
+					// get attributes names and values
+					NamedNodeMap nodeMap = tempNode.getAttributes();
+					for (int i = 0; i < nodeMap.getLength(); i++) {
+						Node node = nodeMap.item(i);
+						text+="\nAtributo: " + node.getNodeName() + "\n	Valor: " + node.getNodeValue();
+						System.out.println(
+								"Atributo: " + node.getNodeName() + "\n	Valor: " + node.getNodeValue());
+					}
+				}
+				
+				if (tempNode.hasChildNodes()) {
+					// loop again if has child nodes
+					printNote(tempNode.getChildNodes(),text);
+				}
+			}
+
+		}
+		return text;
+	}
 }
