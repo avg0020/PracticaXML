b9d6ba6c8f7fc9a2c30f8b39e71feda7352d2b73
avg0020 <avg0020@alu.medac.es>
2023-10-16 18:18:21 +0200

    Finish
    En principio esta todo terminado

--- Practica_XML/src/JFPrincipal.java
+++ Practica_XML/src/JFPrincipal.java
@@ -8,6 +8,7 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
@@ -17,7 +18,10 @@ import javax.swing.JDialog;
 import javax.swing.JFileChooser;
 import java.awt.Font;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.awt.event.ActionEvent;
@@ -25,9 +29,10 @@ import java.awt.event.ActionEvent;
 public class JFPrincipal extends JFrame {
 
 	private static final long serialVersionUID = 1L;
-	private JDmostrarXML jd = new JDmostrarXML(this, true);
+	private JDmostrarXML jdXML = new JDmostrarXML(this, true);
+	private JDmostrarText jdText = new JDmostrarText(this, true);
 	private JPanel contentPane;
-	String ruta = "", xml = "";
+	String rutaXML = "", rutaText = "", rutaSave = "";
 
 	/**
 	 * Launch the application.
@@ -58,10 +63,143 @@ public class JFPrincipal extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 
+		// Botón procesar XML
+		JButton btnProcesar = new JButton("Procesar XML");
+		btnProcesar.setFont(new Font("Comic Sans MS", Font.PLAIN, 19));
+		btnProcesar.setBounds(200, 10, 200, 50);
+		contentPane.add(btnProcesar);
+		btnProcesar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try {
+					// Creamos la instancia de dbf
+					DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+					// Creamos un nuevo documento vacío a partir de dicha instancia
+					DocumentBuilder db = dbf.newDocumentBuilder();
+					// Creamos un objeto File con nuestro fichero XML
+					File fichXML = new File("clases.xml");
+					// Creamos el arbol DOM a partir del fichero
+					Document doc = db.parse(fichXML);
+					// normalize. Elimina nodos vacíos y combina nodos adyacentes en caso de que los
+					// hubiera
+					doc.getDocumentElement().normalize();
+
+					// Elemento raíz
+					System.out.println("Elemento raiz: " + doc.getDocumentElement().getNodeName() + "\n");
+					jdXML.setTextAreaText("Elemento raiz: " + doc.getDocumentElement().getNodeName() + "\n");
+					// Nodo clase
+					NodeList listaClases = doc.getElementsByTagName("clase");
+
+					// Recorremos los nodos de clase
+					for (int i = 0; i < listaClases.getLength(); i++) {
+						// Cada item de la lista es un nodo
+						Node nodo = listaClases.item(i);
+						// Hacemos un casting a Element ya que cada nodo es un Element
+						Element elem = (Element) nodo;
+						// Mostramos el valor del atributo de la etiqueta animal
+						System.out.println("ELemento: " + elem.getNodeName() + " id: " + elem.getAttribute("id"));
+						jdXML.setTextAreaText(
+								"ELemento: " + elem.getNodeName() + " id: " + elem.getAttribute("id") + "\n");
+
+						// clase tiene 4 nodos, nombre, icono, numSpecs y specs
+						NodeList hijosClase = elem.getChildNodes();
+						// Recorremos los hijos de nodos de equipo
+						for (int j = 0; j < hijosClase.getLength(); j++) {
+							Node hijoClase = hijosClase.item(j);
+
+							// Comprobamos tipos de los nodos hijos. Si son element mostramos el valor
+							if (hijoClase.getNodeType() == Node.ELEMENT_NODE
+									&& "nombre".equals(hijoClase.getNodeName())) {
+								System.out.println("	Etiqueta: " + hijoClase.getNodeName() + " Valor: "
+										+ hijoClase.getTextContent());
+								jdXML.setTextAreaText("	Etiqueta: " + hijoClase.getNodeName() + " Valor: "
+										+ hijoClase.getTextContent() + "\n");
+							}
+							if (hijoClase.getNodeType() == Node.ELEMENT_NODE
+									&& "icono".equals(hijoClase.getNodeName())) {
+								System.out.println("	Etiqueta: " + hijoClase.getNodeName() + " Valor: "
+										+ hijoClase.getTextContent());
+								System.out
+										.println("    		Atributo: " + ((Element) hijoClase).getAttribute("enlace"));
+								jdXML.setTextAreaText("	Etiqueta: " + hijoClase.getNodeName() + " Valor: "
+										+ hijoClase.getTextContent() + "\n");
+								jdXML.setTextAreaText(
+										"    		Atributo: " + ((Element) hijoClase).getAttribute("enlace") + "\n");
+							}
+							if (hijoClase.getNodeType() == Node.ELEMENT_NODE
+									&& "numSpecs".equals(hijoClase.getNodeName())) {
+								System.out.println("	Etiqueta: " + hijoClase.getNodeName() + " Valor: "
+										+ hijoClase.getTextContent());
+								jdXML.setTextAreaText("	Etiqueta: " + hijoClase.getNodeName() + " Valor: "
+										+ hijoClase.getTextContent() + "\n");
+							}
+							if (hijoClase.getNodeType() == Node.ELEMENT_NODE
+									&& "specs".equals(hijoClase.getNodeName())) {
+								System.out.println("	Etiqueta: " + hijoClase.getNodeName());
+								jdXML.setTextAreaText("	Etiqueta: " + hijoClase.getNodeName() + "\n");
+
+								// Specs tiene varias spec
+								Element elemSpecs = (Element) hijoClase;
+								NodeList hijosSpecs = elemSpecs.getChildNodes();
+
+								for (int k = 0; k < hijosSpecs.getLength(); k++) {
+									Node hijoSpecs = hijosSpecs.item(k);
+
+									// Comprobamos tipos de los nodos hijos. Si son element mostramos el valor
+									if (hijoSpecs.getNodeType() == Node.ELEMENT_NODE
+											&& "spec".equals(hijoSpecs.getNodeName())) {
+										System.out.println("		Etiqueta: " + hijoSpecs.getNodeName());
+										jdXML.setTextAreaText("		Etiqueta: " + hijoSpecs.getNodeName() + "\n");
+
+										// Spec tiene nombre, rol, facilidad
+										Element elemSpec = (Element) hijoSpecs;
+										NodeList hijosSpec = elemSpec.getChildNodes();
+
+										for (int l = 0; l < hijosSpec.getLength(); l++) {
+											Node hijoSpec = hijosSpec.item(l);
+
+											if (hijoSpec.getNodeType() == Node.ELEMENT_NODE
+													&& "nombre".equals(hijoSpec.getNodeName())) {
+												System.out.println("			Etiqueta: " + hijoSpec.getNodeName()
+														+ " Valor: " + hijoSpec.getTextContent());
+												jdXML.setTextAreaText("			Etiqueta: " + hijoSpec.getNodeName()
+														+ " Valor: " + hijoSpec.getTextContent() + "\n");
+											}
+											if (hijoSpec.getNodeType() == Node.ELEMENT_NODE
+													&& "rol".equals(hijoSpec.getNodeName())) {
+												System.out.println("			Etiqueta: " + hijoSpec.getNodeName()
+														+ " Valor: " + hijoSpec.getTextContent());
+												jdXML.setTextAreaText("			Etiqueta: " + hijoSpec.getNodeName()
+														+ " Valor: " + hijoSpec.getTextContent() + "\n");
+											}
+											if (hijoSpec.getNodeType() == Node.ELEMENT_NODE
+													&& "facilidad".equals(hijoSpec.getNodeName())) {
+												System.out.println("			Etiqueta: " + hijoSpec.getNodeName()
+														+ " Valor: " + hijoSpec.getTextContent());
+												jdXML.setTextAreaText("			Etiqueta: " + hijoSpec.getNodeName()
+														+ " Valor: " + hijoSpec.getTextContent() + "\n");
+											}
+										}
+									}
+								}
+							}
+						}
+
+						System.out.println(" ");
+						jdXML.setTextAreaText("\n");
+					}
+
+					// Mensaje de exito al cargar
+					JOptionPane.showMessageDialog(null, "Archivo cargado correctamente");
+				} catch (ParserConfigurationException | SAXException | IOException ex) {
+					ex.printStackTrace();
+				}
+			}
+		});
+
 		// Botón de cargar archivo xml
 		JButton btnCargar = new JButton("Cargar XML");
 		btnCargar.setFont(new Font("Comic Sans MS", Font.PLAIN, 19));
-		btnCargar.setBounds(200, 101, 200, 50);
+		btnCargar.setBounds(200, 70, 200, 50);
 		contentPane.add(btnCargar);
 		btnCargar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -77,12 +215,13 @@ public class JFPrincipal extends JFrame {
 
 				// Obtiene la ruta si se selecciona un fichero
 				if (resultado == JFileChooser.APPROVE_OPTION) {
-					ruta = fc.getSelectedFile().getAbsolutePath();
+					rutaXML = fc.getSelectedFile().getAbsolutePath();
 				}
-				//Comprueba que se haya puesto un xml
-				if (!ruta.endsWith(".xml")) {
-					//Mensaje de archivo incorrecto
-					JOptionPane.showMessageDialog(null, "Archivo no valido", "Archivo incorrecto", JOptionPane.ERROR_MESSAGE);
+				// Comprueba que se haya puesto un xml
+				if (!rutaXML.endsWith(".xml")) {
+					// Mensaje de archivo incorrecto
+					JOptionPane.showMessageDialog(null, "Archivo no valido", "Archivo incorrecto",
+							JOptionPane.ERROR_MESSAGE);
 				} else {
 					try {
 						// Creamos la instancia de dbf
@@ -90,7 +229,7 @@ public class JFPrincipal extends JFrame {
 						// Creamos un nuevo documento vacío a partir de dicha instancia
 						DocumentBuilder db = dbf.newDocumentBuilder();
 						// Creamos un objeto File con nuestro fichero XML
-						File fichXML = new File(ruta);
+						File fichXML = new File(rutaXML);
 						// Creamos el arbol DOM a partir del fichero
 						Document doc = db.parse(fichXML);
 						// normalize. Elimina nodos vacíos y combina nodos adyacentes en caso de que los
@@ -98,16 +237,16 @@ public class JFPrincipal extends JFrame {
 						doc.getDocumentElement().normalize();
 
 						// Obtenemos el elemento raiz
-						jd.setTextAreaText("Elemento Raiz :" + doc.getDocumentElement().getNodeName());
+						jdXML.setTextAreaText("Elemento Raiz :" + doc.getDocumentElement().getNodeName());
 						System.out.println("Elemento Raiz :" + doc.getDocumentElement().getNodeName());
 						System.out.println("----------");
 
 						// Este método va recorrer el arbol y nos va devolver su contenido
 						if (doc.hasChildNodes()) {
-							printXML(doc.getChildNodes(), jd);
+							printXML(doc.getChildNodes(), jdXML);
 						}
-						
-						//Mensaje de exito al cargar
+
+						// Mensaje de exito al cargar
 						JOptionPane.showMessageDialog(null, "Archivo cargado correctamente");
 					} catch (ParserConfigurationException | SAXException | IOException ex) {
 						ex.printStackTrace();
@@ -119,16 +258,16 @@ public class JFPrincipal extends JFrame {
 		// Botón mostrar la información cargada
 		JButton btnMostrar = new JButton("Mostrar Valores");
 		btnMostrar.setFont(new Font("Comic Sans MS", Font.PLAIN, 19));
-		btnMostrar.setBounds(200, 241, 200, 50);
+		btnMostrar.setBounds(200, 130, 200, 50);
 		contentPane.add(btnMostrar);
 		btnMostrar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				if (ruta.isEmpty()) {
+				if (jdXML.getTextAreaText().isEmpty()) {
 					// Mensaje que se mostrara si no se ha cargado un archivo previamente
 					JOptionPane.showMessageDialog(null, "No hay ningun XML", "Alerta", JOptionPane.ERROR_MESSAGE);
 				} else {
 					// Muestra el JDialog
-					jd.setVisible(true);
+					jdXML.setVisible(true);
 				}
 			}
 		});
@@ -137,7 +276,7 @@ public class JFPrincipal extends JFrame {
 		JButton btnGuardar = new JButton("Guardar Valores");
 		btnGuardar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				if (ruta.isEmpty()) {
+				if (jdXML.getTextAreaText().isEmpty()) {
 					// Mensaje que se mostrara si no se ha cargado un archivo previamente
 					JOptionPane.showMessageDialog(null, "No hay ningun XML", "Alerta", JOptionPane.ERROR_MESSAGE);
 				} else {
@@ -153,9 +292,9 @@ public class JFPrincipal extends JFrame {
 						// Si se ha seleccionado un fichero, si se pulsa cancelar no se ejecuta
 						if (resultado == JFileChooser.APPROVE_OPTION) {
 							// Obtenemos la ruta del fichero
-							String ruta2 = fc.getSelectedFile().getAbsolutePath();
-							FileWriter fw = new FileWriter(new File(ruta2 + ".txt"));
-							fw.write(jd.getTextAreaText());
+							rutaSave = fc.getSelectedFile().getAbsolutePath()+ ".txt";
+							FileWriter fw = new FileWriter(new File(rutaSave));
+							fw.write(jdXML.getTextAreaText());
 							fw.close();
 						}
 					} catch (IOException ex) {
@@ -165,15 +304,98 @@ public class JFPrincipal extends JFrame {
 			}
 		});
 		btnGuardar.setFont(new Font("Comic Sans MS", Font.PLAIN, 19));
-		btnGuardar.setBounds(200, 171, 200, 50);
+		btnGuardar.setBounds(200, 205, 200, 50);
 		contentPane.add(btnGuardar);
+
+		// Botón mostrar valores del txt
+		JButton btnLeerTxt = new JButton("Leer txt");
+		btnLeerTxt.setFont(new Font("Comic Sans MS", Font.PLAIN, 19));
+		btnLeerTxt.setBounds(200, 325, 200, 50);
+		contentPane.add(btnLeerTxt);
+		btnLeerTxt.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				// Ventana de elección de archivos
+				JFileChooser fc = new JFileChooser();
+
+				// Filtro para que solo muestre archivos xml
+				FileNameExtensionFilter filterXml = new FileNameExtensionFilter("Text", "txt");
+				fc.setFileFilter(filterXml);
+
+				// Muestra la ventana de selecionar fichero
+				int resultado = fc.showOpenDialog(null);
+
+				// Obtiene la ruta si se selecciona un fichero
+				if (resultado == JFileChooser.APPROVE_OPTION) {
+					rutaText = fc.getSelectedFile().getAbsolutePath();
+				}
+				// Comprueba que se haya puesto un xml
+				if (!rutaText.endsWith(".txt")) {
+					// Mensaje de archivo incorrecto
+					JOptionPane.showMessageDialog(null, "Archivo no valido", "Archivo incorrecto",
+							JOptionPane.ERROR_MESSAGE);
+				} else {
+					File file = new File(rutaText);
+					try {
+						BufferedReader br = new BufferedReader(new FileReader(file));
+						String texto;
+
+						// Comprueba si se puede leer y lee linea a linea
+						if (file.canRead()) {
+							texto = br.readLine();
+							while (texto != null) {
+								jdText.setTextAreaText(texto+"\n");
+								texto = br.readLine();
+							}
+							br.close();
+							jdText.setVisible(true);
+						} else {
+							System.out.println("No se puede leer");
+						}
+
+					} catch (IOException ex) {
+						ex.printStackTrace();
+					}
+				}
+			}
+		});
+		
+		//Muestra el txt anterior
+		JButton btnTxtAnterior = new JButton("Mostrar txt");
+		btnTxtAnterior.setFont(new Font("Comic Sans MS", Font.PLAIN, 19));
+		btnTxtAnterior.setBounds(200, 265, 200, 50);
+		contentPane.add(btnTxtAnterior);
+		btnTxtAnterior.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				File file = new File(rutaSave);
+				try {
+					BufferedReader br = new BufferedReader(new FileReader(file));
+					String texto;
+
+					// Comprueba si se puede leer y lee linea a linea
+					if (file.canRead()) {
+						texto = br.readLine();
+						while (texto != null) {
+							jdText.setTextAreaText(texto+"\n");
+							texto = br.readLine();
+						}
+						br.close();
+						jdText.setVisible(true);
+					} else {
+						System.out.println("No se puede leer");
+					}
+
+				} catch (IOException ex) {
+					JOptionPane.showMessageDialog(null, "Archivo no disponible", "Archivo incorrecto",
+							JOptionPane.ERROR_MESSAGE);
+				}
+			}
+		});
 
 		// Botón Salir del programa
 		JButton btnSalir = new JButton("Salir");
 		btnSalir.setFont(new Font("Comic Sans MS", Font.PLAIN, 19));
-		btnSalir.setBounds(200, 311, 200, 50);
+		btnSalir.setBounds(200, 403, 200, 50);
 		contentPane.add(btnSalir);
-
 		btnSalir.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				dispose();
